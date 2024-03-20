607035f8907969e026b2d07428c2ebe2f56548ea
Emerson NV <velnae.28@gmail.com>
2024-03-20 09:53:48 -0500

    FEAT agreega docker

---
+++ alias.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+DOCKER_COMPOSE_PATH=".docker"
+source ${DOCKER_COMPOSE_PATH}/.env
+DOCKER_PHP="${APP_NAME}_php"
+
+# php composer
+alias composer="U_ID=${UID} docker exec --user ${UID} -it ${DOCKER_PHP} composer";
+
+# PHP
+alias php="U_ID=${UID} docker exec --user ${UID} -it ${DOCKER_PHP} php";
--- app/Http/Controllers/Backoffice/ExamController.php
+++ app/Http/Controllers/Backoffice/ExamController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Controllers\Backoffice;
 
 use App\Http\Controllers\Controller;
@@ -15,62 +16,60 @@ use App\Models\TSubject;
 use App\Models\TTypeExam;
 use App\Models\TUserExam;
 use App\Validation\ExamValidation;
+use Imagick;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class ExamController extends Controller
 {
     public function actionGetAll(Request $request, $currentPage)
     {
-        $searchParameter=$request->has('searchParameter') ? $request->input('searchParameter') : '';
+        $searchParameter = $request->has('searchParameter') ? $request->input('searchParameter') : '';
 
-        $paginate=PlatformHelper::preparePaginate(TExam::with(['tSubject', 'tGrade', 'tTypeExam'])->whereRaw('compareFind(concat(codeExam, nameExam, descriptionExam, yearExam, keywordExam), ?, 77)=1',[$searchParameter])
-        ->orderby('created_at', 'desc'), 7, $currentPage);
+        $paginate = PlatformHelper::preparePaginate(TExam::with(['tSubject', 'tGrade', 'tTypeExam'])->whereRaw('compareFind(concat(codeExam, nameExam, descriptionExam, yearExam, keywordExam), ?, 77)=1', [$searchParameter])
+            ->orderby('created_at', 'desc'), 7, $currentPage);
 
         return view('backoffice/exam/getall',
-        [
-            'listTExam' => $paginate['listRow'],
-            'currentPage' => $paginate['currentPage'],
-            'quantityPage' => $paginate['quantityPage'],
-            'searchParameter' => $searchParameter
-        ]);
+            [
+                'listTExam' => $paginate['listRow'],
+                'currentPage' => $paginate['currentPage'],
+                'quantityPage' => $paginate['quantityPage'],
+                'searchParameter' => $searchParameter
+            ]);
     }
 
     public function actionInsert(Request $request)
     {
-        if($_POST)
-        {
-            try
-            {
+        if ($_POST) {
+            try {
                 DB::beginTransaction();
 
-                $this->_so->mo->listMessage=(new ExamValidation())->validationInsert($request);
+                $this->_so->mo->listMessage = (new ExamValidation())->validationInsert($request);
 
-                if($this->_so->mo->existsMessage())
-                {
+                if ($this->_so->mo->existsMessage()) {
                     DB::rollBack();
 
                     return PlatformHelper::redirectError($this->_so->mo->listMessage, 'examen/insertar');
                 }
 
                 $tTypeExam = TTypeExam::find($request->input('selectTypeExam'));
-                $tSubject  = TSubject::find($request->input('selectSubject'));
-                $tGrade    = TGrade::find($request->input('selectGrade'));
-                $tDirection= TDirection::find($request->input('selectDirectionExam'));
+                $tSubject = TSubject::find($request->input('selectSubject'));
+                $tGrade = TGrade::find($request->input('selectGrade'));
+                $tDirection = TDirection::find($request->input('selectDirectionExam'));
                 $tDocument = TDocument::whereRaw('key_document=?', ['exam'])->first();
 
-                $tNumberExam = $tTypeExam->numberExecuteYear > 1 ? $request->input('numberEvaluationExecute').'° ': '';
+                $tNumberExam = $tTypeExam->numberExecuteYear > 1 ? $request->input('numberEvaluationExecute') . '° ' : '';
                 $tSiteExam = !$tDirection ? '' : (' ' . $tDirection->nameRegion);
 
-                $tExam=new TExam();
+                $tExam = new TExam();
 
-                $tExam->idExam=uniqid();
+                $tExam->idExam = uniqid();
 
                 $tExam->idTypeExam = $request->input('selectTypeExam');
                 $tExam->idGrade = $request->input('selectGrade');
                 $tExam->idSubject = $request->input('selectSubject');
                 $tExam->idDirection = $request->input('selectDirectionExam') == 'General' ? null : $request->input('selectDirectionExam');
-                $tExam->codeExam = $tDocument->number_document+1;
-                $tExam->nameExam = $tNumberExam.'Evaluación '.strtoupper($tTypeExam->acronymTypeExam).$tSiteExam.' '.$tSubject->nameSubject.' '.$tGrade->numberGrade.'° '.$tGrade->nameGrade;
+                $tExam->codeExam = $tDocument->number_document + 1;
+                $tExam->nameExam = $tNumberExam . 'Evaluación ' . strtoupper($tTypeExam->acronymTypeExam) . $tSiteExam . ' ' . $tSubject->nameSubject . ' ' . $tGrade->numberGrade . '° ' . $tGrade->nameGrade;
                 $tExam->descriptionExam = trim($request->input('txtDescriptionExam'));
                 $tExam->totalPageExam = $request->input('txtTotalPageExam');
                 $tExam->yearExam = $request->input('txtYearExam');
@@ -93,21 +92,32 @@ class ExamController extends Controller
 
                 $tUserExam->save();
 
-                $tDocument->number_document = $tDocument->number_document+1;
+                $tDocument->number_document = $tDocument->number_document + 1;
 
                 $tDocument->save();
+                $filename = $tExam->idExam . '.' . $tExam->extensionExam;
+                $request->file('fileExamExtension')->move(storage_path('/app/file/exam/'), $filename);
 
-                $request->file('fileExamExtension')->move(storage_path('/app/file/exam/'), $tExam->idExam.'.'.$tExam->extensionExam);
+                $pdfPath = storage_path('app/file/exam/' . $filename);
 
-                if ($request->has('txtValueResponseExam'))
-                {
-                    foreach ($request->input('txtValueResponseExam') as $number => $valueResponse)
-                    {
+// Create an Imagick object for the first page of the PDF
+                $imagick = new Imagick($pdfPath . '[0]'); // [0] specifies the first page
+
+// Set the output format to JPEG
+                $imagick->setImageFormat('jpg');
+
+// Save the image to a file
+                $imagePath = storage_path('app/' . $tExam->idExam . '.jpg');
+                $imagick->writeImage($imagePath);
+                dd($imagePath);
+
+                if ($request->has('txtValueResponseExam')) {
+                    foreach ($request->input('txtValueResponseExam') as $number => $valueResponse) {
                         $tAnswer = new TAnswer();
 
                         $tAnswer->idAnswer = uniqid();
                         $tAnswer->idExam = $tExam->idExam;
-                        $tAnswer->numberAnswer = $number+1;
+                        $tAnswer->numberAnswer = $number + 1;
                         $tAnswer->descriptionAnswer = $valueResponse;
 
                         $tAnswer->save();
@@ -122,66 +132,61 @@ class ExamController extends Controller
                 DB::commit();
 
                 return PlatformHelper::redirectCorrect(['Inserción realizada correctamente.'], 'examen/insertar');
-            }
-            catch (\Exception $e)
-            {
+            } catch (\Exception $e) {
                 DB::rollBack();
 
                 return PlatformHelper::catchException(__CLASS__, __FUNCTION__, $e->getMessage(), 'examen/insertar');
             }
         }
 
-        $tTypeExam=TTypeExam::all();
-        $tSubject=TSubject::all();
-        $tGrade=TGrade::all();
-        $tDirection=TDirection::all();
+        $tTypeExam = TTypeExam::all();
+        $tSubject = TSubject::all();
+        $tGrade = TGrade::all();
+        $tDirection = TDirection::all();
 
         return view('backoffice/exam/insert',
-        [
-            'tTypeExam' => $tTypeExam,
-            'tSubject' => $tSubject,
-            'tGrade' => $tGrade,
-            'tDirection' => $tDirection
-        ]);
+            [
+                'tTypeExam' => $tTypeExam,
+                'tSubject' => $tSubject,
+                'tGrade' => $tGrade,
+                'tDirection' => $tDirection
+            ]);
     }
 
     public function actionEdit(Request $request)
     {
-        if($request->has('hdIdExam'))
-        {
-            try
-            {
+        if ($request->has('hdIdExam')) {
+            try {
                 DB::beginTransaction();
 
-                $this->_so->mo->listMessage=(new ExamValidation())->validationEdit($request);
+                $this->_so->mo->listMessage = (new ExamValidation())->validationEdit($request);
 
-                if($this->_so->mo->existsMessage())
-                {
+                if ($this->_so->mo->existsMessage()) {
                     DB::rollBack();
 
                     return PlatformHelper::redirectError($this->_so->mo->listMessage, 'examen/mostrar/1');
                 }
 
-                $tTypeExam  = TTypeExam::find($request->input('selectTypeExam'));
-                $tSubject   = TSubject::find($request->input('selectSubject'));
-                $tGrade     = TGrade::find($request->input('selectGrade'));
+                $tTypeExam = TTypeExam::find($request->input('selectTypeExam'));
+                $tSubject = TSubject::find($request->input('selectSubject'));
+                $tGrade = TGrade::find($request->input('selectGrade'));
                 $tDirection = TDirection::find($request->input('selectDirectionExam'));
 
-                $tNumberExam = $tTypeExam->numberExecuteYear > 1 ? $request->input('numberEvaluationExecute').'° ': '';
+                $tNumberExam = $tTypeExam->numberExecuteYear > 1 ? $request->input('numberEvaluationExecute') . '° ' : '';
                 $tSiteExam = !$tDirection ? '' : (' ' . $tDirection->nameRegion);
 
-                $tExam=TExam::find($request->input('hdIdExam'));
+                $tExam = TExam::find($request->input('hdIdExam'));
 
-                $tExam->idTypeExam=$request->input('selectTypeExam');
-                $tExam->idGrade=$request->input('selectGrade');
-                $tExam->idSubject=$request->input('selectSubject');
+                $tExam->idTypeExam = $request->input('selectTypeExam');
+                $tExam->idGrade = $request->input('selectGrade');
+                $tExam->idSubject = $request->input('selectSubject');
                 $tExam->idDirection = $request->input('selectDirectionExam') == 'General' ? null : $request->input('selectDirectionExam');
-                $tExam->nameExam=$tNumberExam.'Evaluación '.strtoupper($tTypeExam->acronymTypeExam).$tSiteExam.' '.$tSubject->nameSubject.' '.$tGrade->numberGrade.'° '.$tGrade->nameGrade;
-                $tExam->descriptionExam=trim($request->input('txtDescriptionExam'));
-                $tExam->totalPageExam=$request->input('txtTotalPageExam');
-                $tExam->yearExam=$request->input('txtYearExam');
+                $tExam->nameExam = $tNumberExam . 'Evaluación ' . strtoupper($tTypeExam->acronymTypeExam) . $tSiteExam . ' ' . $tSubject->nameSubject . ' ' . $tGrade->numberGrade . '° ' . $tGrade->nameGrade;
+                $tExam->descriptionExam = trim($request->input('txtDescriptionExam'));
+                $tExam->totalPageExam = $request->input('txtTotalPageExam');
+                $tExam->yearExam = $request->input('txtYearExam');
                 $tExam->numberEvaluation = $request->input('numberEvaluationExecute');
-                $tExam->keywordExam=implode('__7SEPARATOR7__', $request->input('selectKeywordExam'));
+                $tExam->keywordExam = implode('__7SEPARATOR7__', $request->input('selectKeywordExam'));
 
                 $tExam->save();
 
@@ -196,68 +201,63 @@ class ExamController extends Controller
 
                 $tUserExam->save();
 
-                if($request->hasFile('fileExamExtension'))
-                {
-                    $tExam=TExam::find($tExam->idExam);
+                if ($request->hasFile('fileExamExtension')) {
+                    $tExam = TExam::find($tExam->idExam);
 
-                    $directoryFiles= storage_path('app/file/exam/'.$tExam->idExam.'.'.$tExam->extensionExam);
+                    $directoryFiles = storage_path('app/file/exam/' . $tExam->idExam . '.' . $tExam->extensionExam);
 
-                    if($tExam->extensionExam!='' && file_exists($directoryFiles)==true)
-                    {
-                        $direccionLink=storage_path('app/file/exam/'.$tExam->idExam.'.'.$tExam->extensionExam);
+                    if ($tExam->extensionExam != '' && file_exists($directoryFiles) == true) {
+                        $direccionLink = storage_path('app/file/exam/' . $tExam->idExam . '.' . $tExam->extensionExam);
 
                         unlink($direccionLink);
                     }
 
-                    $tExam->extensionExam=strtolower($request->file('fileExamExtension')->getClientOriginalExtension());
-                    $tExam->updated_at=date('Y-m-d H:i:s');
+                    $tExam->extensionExam = strtolower($request->file('fileExamExtension')->getClientOriginalExtension());
+                    $tExam->updated_at = date('Y-m-d H:i:s');
 
                     $tExam->save();
 
-                    $request->file('fileExamExtension')->move(storage_path('/app/file/exam/'), $tExam->idExam.'.'.$tExam->extensionExam);
+                    $request->file('fileExamExtension')->move(storage_path('/app/file/exam/'), $tExam->idExam . '.' . $tExam->extensionExam);
                 }
 
                 DB::commit();
 
                 return PlatformHelper::redirectCorrect(['Cambios guardado correctamente.'], 'examen/mostrar/1');
-            }
-            catch (\Exception $e)
-            {
+            } catch (\Exception $e) {
                 DB::rollBack();
 
                 return PlatformHelper::catchException(__CLASS__, __FUNCTION__, $e->getMessage(), 'examen/mostrar/1');
             }
         }
 
-        $tExam=TExam::find($request->input('idExam'));
+        $tExam = TExam::find($request->input('idExam'));
 
-        $tTypeExam=TTypeExam::all();
-        $tSubject=TSubject::all();
-        $tGrade=TGrade::all();
-        $tDirection=TDirection::all();
+        $tTypeExam = TTypeExam::all();
+        $tSubject = TSubject::all();
+        $tGrade = TGrade::all();
+        $tDirection = TDirection::all();
 
-        if($tExam==null)
-        {
+        if ($tExam == null) {
             return PlatformHelper::ajaxDataNoExists();
         }
 
         return view('backoffice/exam/edit',
-        [
-            'tExam' => $tExam,
-            'tTypeExam' => $tTypeExam,
-            'tSubject' => $tSubject,
-            'tGrade' => $tGrade,
-            'tDirection' => $tDirection
-        ]);
+            [
+                'tExam' => $tExam,
+                'tTypeExam' => $tTypeExam,
+                'tSubject' => $tSubject,
+                'tGrade' => $tGrade,
+                'tDirection' => $tDirection
+            ]);
     }
 
     public function actionViewExam($idExam)
     {
-        $tExam=TExam::find($idExam);
+        $tExam = TExam::find($idExam);
 
-        $directoryFiles= storage_path('/app/file/exam/'.$tExam->idExam.'.'.$tExam->extensionExam);
+        $directoryFiles = storage_path('/app/file/exam/' . $tExam->idExam . '.' . $tExam->extensionExam);
 
-        $response=new BinaryFileResponse($directoryFiles);
+        $response = new BinaryFileResponse($directoryFiles);
 
         BinaryFileResponse::trustXSendfileTypeHeader();
 
